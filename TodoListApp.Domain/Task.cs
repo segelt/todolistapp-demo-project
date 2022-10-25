b621f68cbb2259a1757ce17a61c21fe49895bc95
segelt <86981324+segelt@users.noreply.github.com>
2022-10-25 18:41:32 +0300

    add domain

---
+++ TodoListApp.Domain/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoListApp.Domain
+{
+    public class Task
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime? DueDate { get; set; }
+
+        public bool Completed { get; set; } = false;
+    }
+}
