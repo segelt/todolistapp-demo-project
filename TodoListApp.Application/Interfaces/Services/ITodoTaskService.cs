153ab6481b8929fdc32e4a04954985d3b776e200
segelt <86981324+segelt@users.noreply.github.com>
2022-10-25 23:14:49 +0300

    add repo and service contracts

---
+++ TodoListApp.Application/Interfaces/Services/ITodoTaskService.cs
@@ -0,0 +1,26 @@
+﻿using TodoListApp.Domain;
+
+namespace TodoListApp.Application.Interfaces.Services
+{
+    public interface ITodoTaskService
+    {
+        bool CreateTodoTask(CreateTodoTaskRequest createRequest);
+        bool EditTask(EditTodoTaskRequest editRequest);
+        IEnumerable<TodoTask> GetPendingTasks();
+        IEnumerable<TodoTask> GetOverdueTasks();
+    }
+
+    public class CreateTodoTaskRequest
+    {
+        string Title;
+        DateTime? DueDate;
+    }
+
+    public class EditTodoTaskRequest
+    {
+        int id;
+        string Title;
+        DateTime? DueDate;
+        bool? IsCompleted;
+    }
+}
