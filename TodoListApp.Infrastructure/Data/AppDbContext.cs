3cbcc7c17109896d9e2b7ab403050af95eb638f0
segelt <86981324+segelt@users.noreply.github.com>
2022-10-25 19:04:23 +0300

    setup db connection

---
+++ TodoListApp.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace TodoListApp.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() { }
+        public AppDbContext(DbContextOptions opts) : base(opts) { }
+
+        public DbSet<Task> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+    }
+}
---
+++ TodoListApp.Infrastructure/StartupSetup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TodoListApp.Infrastructure.Data;
+
+namespace TodoListApp.Infrastructure
+{
+    public static class StartupSetup
+    {
+        public static void AddDbContext(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.EnableSensitiveDataLogging();
+                options.EnableDetailedErrors();
+                options.UseNpgsql(connectionString);
+            }, ServiceLifetime.Transient);
+        }
+    }
+}
