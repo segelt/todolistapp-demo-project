c8b3afeff1bb50e112a1fb69104fa705b3d02527
segelt <86981324+segelt@users.noreply.github.com>
2022-10-26 18:53:10 +0300

    include overdue task tests

--- TodoListApp.Tests/OverdueTaskTests.cs
+++ TodoListApp.Tests/OverdueTaskTests.cs
@@ -1,4 +1,4 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Moq;
 using TodoListApp.Application.Abstractions;
 using TodoListApp.Application.Implementations.Services;
@@ -9,7 +9,7 @@ using TodoListApp.Infrastructure.Data.Repo;
 namespace TodoListApp.Tests
 {
     [TestClass]
-    public class TaskServiceMocks
+    public class OverdueTaskTests
     {
         private IDateTimeProvider _DateTimeProviderMock;
         private readonly DateTime _PastDate = new DateTime(2022, 10, 24, 0, 0, 0);
@@ -27,7 +27,7 @@ namespace TodoListApp.Tests
         }
 
         [TestMethod]
-        public void TestPendingTasks_Date()
+        public void TestOverdueTasks_ByDate()
         {
             // Arrange
             var todoTasks = new List<TodoTask>()
@@ -36,7 +36,7 @@ namespace TodoListApp.Tests
                 {
                     Id = 1,
                     Title = "Test TodoTask - 1",
-                    DueDate = _FutureDate,
+                    DueDate = _PastDate,
                     Completed = false
                 },
                 new TodoTask
@@ -50,14 +50,14 @@ namespace TodoListApp.Tests
                 {
                     Id = 3,
                     Title = "Test TodoTask - 3",
-                    DueDate = _FutureDate,
+                    DueDate = _PastDate,
                     Completed = false
                 },
                 new TodoTask
                 {
                     Id = 4,
-                    Title = "Test TodoTask - 3",
-                    DueDate = _PastDate,
+                    Title = "Test TodoTask - 4",
+                    DueDate = _FutureDate,
                     Completed = false
                 },
             }.AsQueryable();
@@ -75,11 +75,69 @@ namespace TodoListApp.Tests
             var _sut = new TodoTaskService(repo, _DateTimeProviderMock);
 
             // Act
-            var blogs = _sut.GetPendingTasks();
+            var blogs = _sut.GetOverdueTasks();
 
             // Assert
-            var actualIds = new List<int>() { 1, 2, 3 };
-            Assert.AreEqual(blogs.Count(), 3);
+            var actualIds = new List<int>() { 1, 3 };
+            Assert.AreEqual(blogs.Count(), actualIds.Count);
+            Assert.IsTrue(blogs.Select(p => p.Id).All(id => actualIds.Contains(id)));
+        }
+
+
+        [TestMethod]
+        public void TestOverdueTasks_ByCompleted()
+        {
+            // Arrange
+            var todoTasks = new List<TodoTask>()
+            {
+                new TodoTask
+                {
+                    Id = 1,
+                    Title = "Test TodoTask - 1",
+                    DueDate = _PastDate,
+                    Completed = true
+                },
+                new TodoTask
+                {
+                    Id = 2,
+                    Title = "Test TodoTask - 2",
+                    DueDate = _PastDate,
+                    Completed = false
+                },
+                new TodoTask
+                {
+                    Id = 3,
+                    Title = "Test TodoTask - 3",
+                    DueDate = _PastDate,
+                    Completed = false
+                },
+                new TodoTask
+                {
+                    Id = 4,
+                    Title = "Test TodoTask - 4",
+                    DueDate = _PastDate,
+                    Completed = true
+                },
+            }.AsQueryable();
+            var mockSet = new Mock<DbSet<TodoTask>>();
+
+            mockSet.As<IQueryable<TodoTask>>().Setup(m => m.Provider).Returns(todoTasks.Provider);
+            mockSet.As<IQueryable<TodoTask>>().Setup(m => m.Expression).Returns(todoTasks.Expression);
+            mockSet.As<IQueryable<TodoTask>>().Setup(m => m.ElementType).Returns(todoTasks.ElementType);
+            mockSet.As<IQueryable<TodoTask>>().Setup(m => m.GetEnumerator()).Returns(() => todoTasks.GetEnumerator());
+
+            var mockContext = new Mock<AppDbContext>();
+            mockContext.Setup(m => m.TodoTasks).Returns(mockSet.Object);
+
+            var repo = new TodoTaskRepository(mockContext.Object);
+            var _sut = new TodoTaskService(repo, _DateTimeProviderMock);
+
+            // Act
+            var blogs = _sut.GetOverdueTasks();
+
+            // Assert
+            var actualIds = new List<int>() { 2, 3 };
+            Assert.AreEqual(blogs.Count(), actualIds.Count);
             Assert.IsTrue(blogs.Select(p => p.Id).All(id => actualIds.Contains(id)));
         }
     }
---
+++ TodoListApp.Tests/PendingTaskTests.cs
@@ -0,0 +1,144 @@
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using TodoListApp.Application.Abstractions;
+using TodoListApp.Application.Implementations.Services;
+using TodoListApp.Domain;
+using TodoListApp.Infrastructure.Data;
+using TodoListApp.Infrastructure.Data.Repo;
+
+namespace TodoListApp.Tests
+{
+    [TestClass]
+    public class PendingTaskTests
+    {
+        private IDateTimeProvider _DateTimeProviderMock;
+        private readonly DateTime _PastDate = new DateTime(2022, 10, 24, 0, 0, 0);
+        private readonly DateTime _FutureDate = new DateTime(2022, 10, 26, 0, 0, 0);
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            Mock<IDateTimeProvider> mockDateTimeProvider = new Mock<IDateTimeProvider>();
+            DateTime currentDate = new DateTime(2022, 10, 25, 0, 0, 0);
+            mockDateTimeProvider.Setup(p => p.Now()).Returns(currentDate);
+
+            _DateTimeProviderMock = mockDateTimeProvider.Object;
+        }
+
+        [TestMethod]
+        public void TestPendingTasks_ByDate()
+        {
+            // Arrange
+            var todoTasks = new List<TodoTask>()
+            {
+                new TodoTask
+                {
+                    Id = 1,
+                    Title = "Test TodoTask - 1",
+                    DueDate = _FutureDate,
+                    Completed = false
+                },
+                new TodoTask
+                {
+                    Id = 2,
+                    Title = "Test TodoTask - 2",
+                    DueDate = _FutureDate,
+                    Completed = false
+                },
+                new TodoTask
+                {
+                    Id = 3,
+                    Title = "Test TodoTask - 3",
+                    DueDate = _FutureDate,
+                    Completed = false
+                },
+                new TodoTask
+                {
+                    Id = 4,
+                    Title = "Test TodoTask - 4",
+                    DueDate = _PastDate,
+                    Completed = false
+                },
+            }.AsQueryable();
+            var mockSet = new Mock<DbSet<TodoTask>>();
+
+            mockSet.As<IQueryable<TodoTask>>().Setup(m => m.Provider).Returns(todoTasks.Provider);
+            mockSet.As<IQueryable<TodoTask>>().Setup(m => m.Expression).Returns(todoTasks.Expression);
+            mockSet.As<IQueryable<TodoTask>>().Setup(m => m.ElementType).Returns(todoTasks.ElementType);
+            mockSet.As<IQueryable<TodoTask>>().Setup(m => m.GetEnumerator()).Returns(() => todoTasks.GetEnumerator());
+
+            var mockContext = new Mock<AppDbContext>();
+            mockContext.Setup(m => m.TodoTasks).Returns(mockSet.Object);
+
+            var repo = new TodoTaskRepository(mockContext.Object);
+            var _sut = new TodoTaskService(repo, _DateTimeProviderMock);
+
+            // Act
+            var blogs = _sut.GetPendingTasks();
+
+            // Assert
+            var actualIds = new List<int>() { 1, 2, 3 };
+            Assert.AreEqual(blogs.Count(), actualIds.Count);
+            Assert.IsTrue(blogs.Select(p => p.Id).All(id => actualIds.Contains(id)));
+        }
+
+
+        [TestMethod]
+        public void TestPendingTasks_ByCompleted()
+        {
+            // Arrange
+            var todoTasks = new List<TodoTask>()
+            {
+                new TodoTask
+                {
+                    Id = 1,
+                    Title = "Test TodoTask - 1",
+                    DueDate = _FutureDate,
+                    Completed = true
+                },
+                new TodoTask
+                {
+                    Id = 2,
+                    Title = "Test TodoTask - 2",
+                    DueDate = _FutureDate,
+                    Completed = false
+                },
+                new TodoTask
+                {
+                    Id = 3,
+                    Title = "Test TodoTask - 3",
+                    DueDate = _FutureDate,
+                    Completed = false
+                },
+                new TodoTask
+                {
+                    Id = 4,
+                    Title = "Test TodoTask - 4",
+                    DueDate = _FutureDate,
+                    Completed = true
+                },
+            }.AsQueryable();
+            var mockSet = new Mock<DbSet<TodoTask>>();
+
+            mockSet.As<IQueryable<TodoTask>>().Setup(m => m.Provider).Returns(todoTasks.Provider);
+            mockSet.As<IQueryable<TodoTask>>().Setup(m => m.Expression).Returns(todoTasks.Expression);
+            mockSet.As<IQueryable<TodoTask>>().Setup(m => m.ElementType).Returns(todoTasks.ElementType);
+            mockSet.As<IQueryable<TodoTask>>().Setup(m => m.GetEnumerator()).Returns(() => todoTasks.GetEnumerator());
+
+            var mockContext = new Mock<AppDbContext>();
+            mockContext.Setup(m => m.TodoTasks).Returns(mockSet.Object);
+
+            var repo = new TodoTaskRepository(mockContext.Object);
+            var _sut = new TodoTaskService(repo, _DateTimeProviderMock);
+
+            // Act
+            var blogs = _sut.GetPendingTasks();
+
+            // Assert
+            var actualIds = new List<int>() { 2, 3 };
+            Assert.AreEqual(blogs.Count(), actualIds.Count);
+            Assert.IsTrue(blogs.Select(p => p.Id).All(id => actualIds.Contains(id)));
+        }
+    }
+}
